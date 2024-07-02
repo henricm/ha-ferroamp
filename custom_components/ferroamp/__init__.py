612ebae491c121330f41a3755f72f9649236f2f9
Joakim Olsson <joakim@unbound.se>
2024-07-02 14:32:16 +0200

    chore: await hass.async_create_task

--- custom_components/ferroamp/__init__.py
+++ custom_components/ferroamp/__init__.py
@@ -32,7 +32,7 @@ async def async_setup_entry(
     ]
 
     # Forward the setup to the sensor platform.
-    hass.async_create_task(
+    await hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
