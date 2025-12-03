d79a7e7178e4623525703201790188d44b467cd6
Joakim Olsson <joakim@unbound.se>
2025-12-03 07:42:50 +0100

    fix: simplify options flow handler initialization
    Removes the unnecessary config_entry parameter from 
    the FerroampOptionsFlowHandler constructor. This 
    simplifies the code and improves readability by 
    directly passing the config_entry in the static method.
    
    Fixes #824

--- custom_components/ferroamp/config_flow.py
+++ custom_components/ferroamp/config_flow.py
@@ -41,13 +41,10 @@ class FerroampConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return FerroampOptionsFlowHandler(config_entry)
+        return FerroampOptionsFlowHandler()
 
 
 class FerroampOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         errors: Dict[str, str] = {}
         if user_input is not None:
